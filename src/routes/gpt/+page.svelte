17a994a07d2416b71439961a2ef78e0db930dc2d
amritbrhat <i.amrit.p@gmail.com>
2023-05-11 04:34:55 +0530

    sort

--- src/routes/gpt/+page.svelte
+++ src/routes/gpt/+page.svelte
@@ -2,33 +2,11 @@
 		
 	import visibilityMode from '$lib/stores/visibility'	
 	import GPTBox from '$lib/agents/GPTBox.svelte'
-	import { allconvos } from '$lib/utils/convos'
 
 	let fake = false
 	let isChat = false
 	let password = ''
 	let input: HTMLInputElement
-	let openItem = Array(10).fill(false)
-
-	function getContentParts(mapping:any) {
-		let parts = []
-		for ( let key in mapping ) {
-			let message = mapping[key].message
-			if ( message && message.content && message.content.parts) {
-				parts.push(...message.content.parts)
-			}
-		}
-		return parts
-	}	
-
-	function toggleOpenItem(index:number) {
-		openItem[index] = !openItem[index]
-		for ( let i = 0; i < openItem.length; i ++ ) {
-			if ( i !== index && openItem[i] === true ) {
-				openItem[i] = false
-			}
-		}
-	}
 
 	const keyed = import.meta.env.VITE_OWN_PUBLIC
 
