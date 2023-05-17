de13e2b10c3645c32af9e173c15bc25532ee98e6
amritbrhat <i.amrit.p@gmail.com>
2023-05-18 02:29:02 +0530

    verceladaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 import remarkFrontmatter from 'remark-frontmatter';
@@ -16,9 +16,6 @@ const config = {
 	],
 	extensions: ['.svelte', '.md', '.svx'],
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
